67dd24e1bb547a26e910990441cfb5158eb10083
unknown <your-email@example.com>
2025-09-18 10:04:16 +0530

    Updated layout for better responsive view

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -37,7 +37,7 @@ const HeroSlider = () => {
         h-[500px]    /* 📱 Mobile default */
         sm:h-[500px] /* 📲 Small screens */
         md:h-[600px] /* 💻 Medium */
-        lg:h-[1000px] /* 🖥 Desktop */
+        lg:h-[800px] /* 🖥 Desktop */
         overflow-hidden
       "
     >
